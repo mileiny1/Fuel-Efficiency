7abae798aa4c48a8c1f8c54f5b4c757565837b6e
Mileiny Nolasco <149266977+mileiny1@users.noreply.github.com>
2024-09-25 20:29:08 -0400

    Add files via upload

---
+++ miles driven.py
@@ -0,0 +1,5 @@
+#Miles Driven
+milesDriven = float(input("please enter the mileage:"))
+galonsofGasUsed = float(input("please enter the gallon of gas used:"))
+milesPerGalon = milesDriven/galonsofGasUsed
+print("The car's miles-galon is: " + str(milesPerGalon))
